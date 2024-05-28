7d24a72c1c04e953eadd1d4490f1967f63cf0384
djdagostino <152912672+daviddagWGELD@users.noreply.github.com>
2024-05-28 15:10:59 -0400

    Edited Input Box

--- TestClientServer.Client/obj/Debug/net8.0/TestClientServer.Client.AssemblyInfo.cs
+++ TestClientServer.Client/obj/Debug/net8.0/TestClientServer.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestClientServer.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db7465a9859fd738daa792a4b9c6e01ac31048ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12fc3d73d19e2fe48780839ac92389c62dd3523f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestClientServer.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestClientServer.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
