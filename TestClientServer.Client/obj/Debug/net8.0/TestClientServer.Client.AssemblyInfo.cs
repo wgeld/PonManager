c29943eb891bebe5058acf3ea103cff73e140409
djdagostino <152912672+daviddagWGELD@users.noreply.github.com>
2024-05-28 11:52:10 -0400

    EDIT: Added styling to the pon form.

--- TestClientServer.Client/obj/Debug/net8.0/TestClientServer.Client.AssemblyInfo.cs
+++ TestClientServer.Client/obj/Debug/net8.0/TestClientServer.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestClientServer.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bfd77e98771f74338dbd346abbdeb38f200c5b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eca47891ade83f2a837e9ed800a3e346af316fa4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestClientServer.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestClientServer.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
