c932b5828d26d5ce2fb046f5f7ab8b0fb5ad42e4
djdagostino <152912672+daviddagWGELD@users.noreply.github.com>
2024-06-12 13:27:46 -0400

    V1 - Pon Light Up

--- TestClientServer.Server/Data/Services/UtilityService.cs
+++ TestClientServer.Server/Data/Services/UtilityService.cs
@@ -1,5 +1,4 @@
 ﻿using TestClientServer.Server.Data.Interfaces;
-using TestClientServer.Shared.Models;
 using TestClientServer.Shared.Models.Server;
 
 namespace TestClientServer.Server.Data.Services;
--- TestClientServer.Server/obj/Debug/net8.0/TestClientServer.Server.AssemblyInfo.cs
+++ TestClientServer.Server/obj/Debug/net8.0/TestClientServer.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestClientServer.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dd94428ff9945b9ee67fe7a5474e5ab7b22665d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39948e932fb0af1da7413d2a569f325d17ba742c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestClientServer.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestClientServer.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
