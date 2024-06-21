b48d30bbc6ab7ca409dbcbb3c76f8cc13b25872a
djdagostino <152912672+daviddagWGELD@users.noreply.github.com>
2024-06-21 13:16:23 -0400

    Added gitignore

--- TestClientServer.Client/obj/Debug/net8.0/TestClientServer.Client.AssemblyInfo.cs
+++ TestClientServer.Client/obj/Debug/net8.0/TestClientServer.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestClientServer.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c86459b99a086e9a95e911d6462f0db6cc90a12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2302554cbf0a6965750546742a06fa9e8fc29058")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestClientServer.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestClientServer.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
