943a3faed45a43e1716692c3a8fb80e975dfbdea
djdagostino <152912672+daviddagWGELD@users.noreply.github.com>
2024-06-21 10:57:17 -0400

    Unversioned Files Push

--- TestClientServer.Client/obj/Debug/net8.0/TestClientServer.Client.AssemblyInfo.cs
+++ TestClientServer.Client/obj/Debug/net8.0/TestClientServer.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestClientServer.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+856206464000beadcd9e35d2cf45d91c6812f7c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c86459b99a086e9a95e911d6462f0db6cc90a12")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestClientServer.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestClientServer.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
